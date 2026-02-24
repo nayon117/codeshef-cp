ad228768e1b9fe172150628cfed4279aa9f8d8c4
nayon117 <hasibul.nayon1@gmail.com>
2026-02-24 11:35:08 +0600

    solve assignment problem

---
+++ Subscriptions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve () {
+   double n, x; cin >> n >> x;
+   double r = ceil(n / 6);
+   cout << r * x << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) solve ();
+    return 0;
+}
