6ab30c3f9a3b5e82ce3cc2474965b06da0bb26dc
nayon117 <hasibul.nayon1@gmail.com>
2026-02-25 14:51:27 +0600

    solve chefland problem

---
+++ Chefland_Games.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve () {
+   int a, b, c, d; cin >> a >> b >> c >> d;
+   if(a + b + c + d == 0) cout << "IN\n";
+   else cout << "OUT\n";  
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) solve ();
+    return 0;
+}
