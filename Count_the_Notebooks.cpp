55e166a60dc973d7a9b2903f8ff8882e4107f48e
nayon117 <hasibul.nayon1@gmail.com>
2026-02-26 11:37:46 +0600

    solve notebook problem

---
+++ Count_the_Notebooks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve () {
+   int n; cin >> n;
+   cout << n * 10 << '\n';  
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) solve ();
+    return 0;
+}
