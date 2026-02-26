bab1141ed2a883bfc36bf636f6c59c6204cfcb58
nayon117 <hasibul.nayon1@gmail.com>
2026-02-26 13:01:36 +0600

    solve temperature problem

---
+++ Air_Conditioner_Temperature.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve () {
+   int a, b, c; cin >> a >> b >> c;
+   int mx = max ({a, c});
+   if(mx <= b) cout << "Yes\n";
+   else cout << "No\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) solve ();
+    return 0;
+}
